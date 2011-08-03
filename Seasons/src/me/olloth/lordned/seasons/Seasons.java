90dd3b221b52df5e0f0ab107724b12b2a1a22a79
Dalton Pelc <djpelc@mtu.edu>
2011-08-02 21:47:05 -0400

    Basic plugin

--- Seasons/src/me/olloth/lordned/seasons/Seasons.java
+++ Seasons/src/me/olloth/lordned/seasons/Seasons.java
@@ -1,16 +1,36 @@
 package me.olloth.lordned.seasons;
 
+import java.io.File;
+
+import me.olloth.lordned.seasons.util.Config;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Seasons extends JavaPlugin {
 
+	private PluginDescriptionFile info;
+	private PluginManager pm;
+	private File directory, config;
+
 	public void onDisable() {
-		
+
+		System.out.println("[" + info.getName() + "] disabled.");
+
 	}
 
-
 	public void onEnable() {
-		
+
+		info = getDescription();
+		pm = getServer().getPluginManager();
+		directory = getDataFolder();
+
+		Config.configSetup(directory, config);
+
+		System.out.println("[" + info.getName() + "] version "
+				+ info.getVersion() + " enabled.");
+
 	}
 
 }
