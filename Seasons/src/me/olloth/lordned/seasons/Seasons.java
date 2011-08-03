70e1bbbc0739ad563b9fa7a7fd44812898227441
Dalton Pelc <djpelc@mtu.edu>
2011-08-02 21:28:47 -0400

    first java file

---
+++ Seasons/src/me/olloth/lordned/seasons/Seasons.java
@@ -0,0 +1,16 @@
+package me.olloth.lordned.seasons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Seasons extends JavaPlugin {
+
+	public void onDisable() {
+		
+	}
+
+
+	public void onEnable() {
+		
+	}
+
+}
