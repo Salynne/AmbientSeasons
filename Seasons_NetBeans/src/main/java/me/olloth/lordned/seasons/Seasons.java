af88a290c0e590a9a6d3289dfb726d5a8b85d74f
LordNed <lord.matt.hoffman@gmail.com>
2011-08-02 19:43:06 -0700

    Added Netbeans project again.

---
+++ Seasons_NetBeans/src/main/java/me/olloth/lordned/seasons/Seasons.java
@@ -0,0 +1,16 @@
+package me.olloth.lordned.seasons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Seasons extends JavaPlugin {
+    public void onDisable() {
+        // TODO: Place any custom disable code here.
+        System.out.println(this + " is now disabled!");
+    }
+
+    public void onEnable() {
+        // TODO: Place any custom enable code here, such as registering events
+        
+        System.out.println(this + " is now enabled!");
+    }
+}
