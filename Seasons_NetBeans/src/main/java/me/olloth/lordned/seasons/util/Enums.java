b99d67fb8e59a5f37949c06fd5011b780e462766
LordNed <lord.matt.hoffman@gmail.com>
2011-08-02 22:27:44 -0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Seasons_NetBeans/src/main/java/me/olloth/lordned/seasons/Seasons.java

--- Seasons_NetBeans/src/main/java/me/olloth/lordned/seasons/util/Enums.java
+++ Seasons_NetBeans/src/main/java/me/olloth/lordned/seasons/util/Enums.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package me.olloth.lordned.seasons.Utils;
+package me.olloth.lordned.seasons.util;
 
 /**
  *
