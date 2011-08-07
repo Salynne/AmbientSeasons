d95eff61a5c6b7c6874e0fcab2c641aad5cfd6b0
Dalton Pelc <djpelc@mtu.edu>
2011-08-06 20:21:10 -0400

    going to make the hud more efficient

---
+++ Seasons_NetBeans/src/main/java/me/ambientseasons/HUDLabel.java
@@ -0,0 +1,11 @@
+package me.ambientseasons;
+
+import org.getspout.spoutapi.gui.GenericLabel;
+
+public class HUDLabel extends GenericLabel {
+@Override
+public boolean isDirty() {
+	// TODO Auto-generated method stub
+	return super.isDirty();
+}
+}
